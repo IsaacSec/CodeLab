d3a8057f643558f3c7c9f86ba39e08a7918be309
IsaacSec <isaacsecundino3@gmail.com>
2017-04-07 01:33:33 -0500

    clean

--- data-structures/stack.c
+++ data-structures/stack.c
@@ -1,32 +1,32 @@
 /*  Chapter 1: Object-Oriented Programming with Classes
-    
+
     ABSTRACTION
 
     Abstraction is the creation of a well-defined interface for an object.
-    Proper abstraction separates the implementation of an object from its 
-    interface. 
-        In C, abstraction involves wrapping a data structure with 
+    Proper abstraction separates the implementation of an object from its
+    interface.
+        In C, abstraction involves wrapping a data structure with
         a functional interface.
 
     ENCAPSULATION
 
-    Encapsulation means keeping the implementation details of your 
-    abstractions private. 
+    Encapsulation means keeping the implementation details of your
+    abstractions private.
         In C, you keep details private by hiding them in a separate file.
 
     HIERARCHY
 
     Where one object uses others in its implementation is called "composition"
-    because we compose larger objects out of smaller ones. 
-    The other kind of hierarchy found in OOP languages is called "derivation" 
-    (or inheritance). Derivation allows an abstraction to reuse not only the 
+    because we compose larger objects out of smaller ones.
+    The other kind of hierarchy found in OOP languages is called "derivation"
+    (or inheritance). Derivation allows an abstraction to reuse not only the
     implementation, but also the interface of another abstraction.
 
     POLYMORPHISM
 
-    Code is said to be polymorphic if it can be transparently used on 
-    instances of different types. Polymorphism allows us to write code in 
-    terms of a generic class and have it work correctly for any actual class. 
+    Code is said to be polymorphic if it can be transparently used on
+    instances of different types. Polymorphism allows us to write code in
+    terms of a generic class and have it work correctly for any actual class.
 
 
 */
@@ -37,11 +37,12 @@
 
 #include "stack.h"
 
-/* This functios are only visible for stack.c also called 
+/* This functios are only visible for stack.c also called
 *  private functions
 */
 
-int compare (const void *a, const void *b){
+int compare (const void *a, const void *b)
+{
     int int_a = *((int*)(a));
     int int_b = *((int*)(b));
 
@@ -54,7 +55,8 @@ int compare (const void *a, const void *b){
     }
 }
 
-int * arraycpy (const int * src, int length){
+int * arraycpy (const int * src, int length)
+{
     int *cpy = (int *) malloc(sizeof(int) * length);
     for (int i=0; i<length; i++)
     {
@@ -66,12 +68,14 @@ int * arraycpy (const int * src, int length){
 /* End of private functions */
 
 /* Inititalize the stack */
-void init (Stack *s){
+void init (Stack *s)
+{
     s->top = 0;
 }
 
 /* Push onto the stack */
-void push (Stack *s, int i){
+void push (Stack *s, int i)
+{
     if (s->top == STACKSIZE){
         printf("Stack error: Limit Reached\n");
     } else{
@@ -80,19 +84,22 @@ void push (Stack *s, int i){
 }
 
 /* Pop an element off the stack */
-int pop (Stack *s){
+int pop (Stack *s)
+{
     return s->items[--s->top];
 }
 
 /* Clean up the stack when it is not required anymore */
-void cleanup (Stack *s){
+void cleanup (Stack *s)
+{
     do{
         s->items[s->top--] = 0;
     }while (s->top != 0);
 }
 
 /* Prints all the elements in the stack */
-void print (const Stack *s){
+void print (const Stack *s)
+{
     if (s->top == 0){
         printf("[Empty]\n");
         return;
@@ -112,7 +119,8 @@ void print (const Stack *s){
 
 /* New functions */
 
-int mean (StatStack *s){
+int mean (StatStack *s)
+{
     int total = 0;
     int index = 0;
 
@@ -127,7 +135,8 @@ int mean (StatStack *s){
     return total/s->basic_stack.top;
 }
 
-int median (StatStack *s){
+int median (StatStack *s)
+{
     Stack *stack = &s->basic_stack;
     int size = stack->top;
     int *items = arraycpy(&s->basic_stack.items[0], size);
@@ -145,7 +154,8 @@ int median (StatStack *s){
     return median_value;
 }
 
-int mode (StatStack *s){
+int mode (StatStack *s)
+{
     Stack *stack = &s->basic_stack;
     int size = stack->top;
     int *items = arraycpy(&s->basic_stack.items[0], size);
@@ -173,22 +183,27 @@ int mode (StatStack *s){
 
 /* Inherited functions*/
 
-void ss_init (StatStack *s){
+void ss_init (StatStack *s)
+{
     init (&s->basic_stack);
 }
 
-void ss_push (StatStack *s, int i){
+void ss_push (StatStack *s, int i)
+{
     push (&s->basic_stack, i);
 }
 
-void ss_pop (StatStack *s){
+void ss_pop (StatStack *s)
+{
     pop (&s->basic_stack);
 }
 
-void ss_cleanup (StatStack *s){
+void ss_cleanup (StatStack *s)
+{
     cleanup (&s->basic_stack);
 }
 
-void ss_print (const StatStack *s){
+void ss_print (const StatStack *s)
+{
     print (&s->basic_stack);
 }
